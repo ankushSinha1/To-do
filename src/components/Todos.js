b0479eb6604385fefc1814cd7f1560e5f0f71405
Ankush Sinha <97420654+ankushSinha1@users.noreply.github.com>
2024-01-14 23:22:04 +0530

    Initial commit

---
+++ src/components/Todos.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { addTodos } from "../redux/reducer";
+import { GoPlus } from "react-icons/go";
+import { motion } from "framer-motion";
+
+const Todos = (props) => {
+  const [todo, setTodo] = useState("");
+  const add = () => {
+    if (todo === "") {
+      alert("Input is Empty");
+    } else {
+      props.addTodo({
+        id: Math.floor(Math.random() * 1000),
+        item: todo,
+        completed: false,
+      });
+      setTodo("");
+    }
+  };
+  //console.log("props from store", props);
+  return (
+    <div className="addTodos">
+      <input
+        type="text"
+        onChange={(e) => setTodo(e.target.value)}
+        className="todo-input"
+        value={todo}
+      />
+      <button onClick={() => add()}> Add</button>
+      <br />
+    </div>
+  );
+};
+const mapStateToProps = (state) => {
+  return {todos: state};
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addTodo: (obj) => dispatch(addTodos(obj)),
+  };
+};
+
+//we can use connect method to connect this component with redux store
+export default connect(mapStateToProps, mapDispatchToProps)(Todos);
